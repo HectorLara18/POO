ca9c105ae42adec670f04e91a303959c4b208302
hlara <hectorlaras.18@gmail.com>
2021-02-22 11:25:50 -0600

    se agregan dos objetor a main.java

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,16 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        System.out.println("Car License: " + car.license);
+
+        Car carro = new Car();
+        carro.license = "HGF-4567";
+        carro.driver = "Hector Lara";
+        carro.passenger = 5;
+        System.out.println("Nombre del Conductor: " + carro.driver);
     }
 }
