b0204470a2f95f853e2478da826b45a692f06154
hlara <hectorlaras.18@gmail.com>
2021-02-22 11:42:55 -0600

    se agregan dos objetos a main.py

--- Java/Main.java
+++ Java/Main.java
@@ -5,12 +5,12 @@ class Main {
         car.license = "AMQ123";
         car.driver = "Andres Herrera";
         car.passenger = 4;
-        System.out.println("Car License: " + car.license);
+        car.printDataCar();
 
         Car carro = new Car();
         carro.license = "HGF-4567";
         carro.driver = "Hector Lara";
         carro.passenger = 5;
-        System.out.println("Nombre del Conductor: " + carro.driver);
+        carro.printDataCar();
     }
 }
--- python/main.py
+++ python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__=="__main__":
     print("Hola Mundo con Python")
+    carro1 = Car()
+    carro1.license = "AMS234"
+    carro1.driver = "Hector Lara"
+    print(vars(carro1))
+
+    carro = Car()
+    carro.driver = "Raul  Lara"
+    carro.license = "asdf-1234"
+    print(vars(carro))
