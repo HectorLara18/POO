42ae99b4d8f64719c21ae755a6e4b85fcdda0d1e
hector lara <hectorlaras.18@gmail.com>
2021-02-22 16:50:15 -0600

    se agran objetos a java

--- Java/Main.java
+++ Java/Main.java
@@ -8,5 +8,9 @@ class Main {
         Car carro = new Car("HGF-4567", new Account("Hector Lara", "HL1234"));
         carro.passenger = 5;
         carro.printDataCar();
+
+        UberPool Carro2 = new UberPool("HLS1234", new Account("Hector Lara", "HL1234"), "Honda", "City");
+        Carro2.printDataCar();
+        Carro2.printBrand();
     }
 }
